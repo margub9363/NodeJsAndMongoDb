b95cf03d6b9444261bdb93da816273f816518312
Rahman <margub.893@gmail.com>
2021-03-25 11:35:34 +0530

    11

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const http = require("http");
 
 // Synchronous read/write (Blocking code)
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -9,22 +10,33 @@ const fs = require("fs");
 // console.log("File has been created");
 
 // Asynchronous read/write , Non-Blocking codes
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  console.log(data1);
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/start.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   console.log(data1);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/start.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile(
-        "./txt/final.txt",
-        `${data2}\n\n ${data3}`,
-        "utf-8",
-        (err) => {
-          console.log("Your file has been written");
-        }
-      );
-    });
-  });
+//       fs.writeFile(
+//         "./txt/final.txt",
+//         `${data2}\n\n ${data3}`,
+//         "utf-8",
+//         (err) => {
+//           console.log("Your file has been written");
+//         }
+//       );
+//     });
+//   });
+// });
+// console.log("Will Read a file!");
+
+// Server
+const server = http.createServer((req, res) => {
+  console.log(req);
+  console.log(res);
+  res.end("Hello from server!");
 });
-console.log("Will Read a file!");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
