598f9b8cf67210e4a400d8f6511857a9d807ef9a
Rahman <margub.893@gmail.com>
2021-03-26 10:56:04 +0530

    16 Parsing Values from URL

--- complete-node-bootcamp-master/1-node-farm/starter/index.js
+++ complete-node-bootcamp-master/1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 // Synchronous read/write (Blocking code)
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
@@ -69,9 +70,11 @@ const dataObj = JSON.parse(data);
 // console.log(dataObj);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  // console.log(req);
+  const { query, pathname } = url.parse(req.url, true);
+  // const pathname = req.url;
   // Overview Page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
@@ -80,11 +83,15 @@ const server = http.createServer((req, res) => {
     res.end(output);
   }
   //  Product Page
-  else if (pathName === "/product") {
-    res.end("This is the product page");
+  else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    console.log(query);
+    res.end(output);
   }
   // Api Page
-  else if (pathName === "/api") {
+  else if (pathname === "/api") {
     fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data4) => {
       const textFileContent = JSON.parse(data4);
       console.log(textFileContent);
