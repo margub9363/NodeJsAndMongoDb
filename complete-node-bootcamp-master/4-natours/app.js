c5175cf43ce2108815a0483310d36c96e7afac50
margub9363 <margub.9363@gmail.com>
2021-05-23 08:39:50 +0530

    some eslint issue resolved (from previous commit)

--- complete-node-bootcamp-master/4-natours/app.js
+++ complete-node-bootcamp-master/4-natours/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const app = express();
 const morgan = require('morgan');
-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
