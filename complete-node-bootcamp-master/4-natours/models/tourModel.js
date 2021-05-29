3a10a9dd64abbafe374a790d93ba328328e33fe4
margub9363 <margub.9363@gmail.com>
2021-05-29 11:21:38 +0530

    92 Modeled the tour

--- complete-node-bootcamp-master/4-natours/models/tourModel.js
+++ complete-node-bootcamp-master/4-natours/models/tourModel.js
@@ -5,14 +5,50 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a name'],
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    requirred: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A Tour must have a price'],
   },
+  priceDiscout: Number,
+  summary: {
+    type: String,
+    trim: true, //it will remove the multiple white spaces
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
