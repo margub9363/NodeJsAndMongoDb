ec421fcd93253afb503b2beeaf34218e7a4cf41c
margub9363 <margub.9363@gmail.com>
2021-06-01 18:34:34 +0530

    114 Error Refactoring

--- complete-node-bootcamp-master/4-natours/app.js
+++ complete-node-bootcamp-master/4-natours/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -26,26 +28,10 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Cant find ${req.originalUrl} on this server `,
-  // });
-
-  const err = new Error(`Cant find ${req.originalUrl} on this server`);
-  err.status = 'fail';
-  err.statusCode = '404';
-
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
   next(err);
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
