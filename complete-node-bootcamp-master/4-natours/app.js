0e56ef0f36a725156ba69c2af4ccaa2cb9f6403e
margub9363 <margub.9363@gmail.com>
2021-06-01 17:57:14 +0530

    111 Handling un-handled routes

--- complete-node-bootcamp-master/4-natours/app.js
+++ complete-node-bootcamp-master/4-natours/app.js
@@ -16,11 +16,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 😄');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requsetTime = new Date().toISOString();
   next();
 });
@@ -29,5 +24,12 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cant find ${req.originalUrl} on this server `,
+  });
+});
 
 module.exports = app;
