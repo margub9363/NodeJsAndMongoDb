ac695fcb31af382b37d8b6426d6d61514640c07d
margub9363 <margub.9363@gmail.com>
2021-06-02 15:47:41 +0530

    122 Handling Un-handled rejection
    Like wrong DB password or server down

--- complete-node-bootcamp-master/4-natours/server.js
+++ complete-node-bootcamp-master/4-natours/server.js
@@ -25,6 +25,14 @@ mongoose
 console.log(process.env);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}.....`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled Rejection 💣  Shutting Down');
+  server.close(() => {
+    process.exit(1);
+  });
 });
