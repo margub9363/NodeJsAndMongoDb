960536999b367f36438fecccb6845603bdb7f2cb
margub9363 <margub.9363@gmail.com>
2021-06-08 17:28:21 +0530

    139 Deleting the current user

--- complete-node-bootcamp-master/4-natours/controllers/userController.js
+++ complete-node-bootcamp-master/4-natours/controllers/userController.js
@@ -38,7 +38,7 @@ exports.UpdateMe = catchAsync(async (req, res, next) => {
   }
   // 2) Filtered out unwanted fields name that are not allowed to be updated
   const filteredBody = filterObj(req.body, 'name', 'email');
-  // 2)Update user document
+  // 3)Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     new: true,
     runValidators: true,
@@ -62,6 +62,15 @@ exports.createNewUser = (req, res) => {
     message: 'The route is not yet defined',
   });
 };
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
