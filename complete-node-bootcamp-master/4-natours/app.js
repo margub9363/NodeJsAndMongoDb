bdb01996f7538ebb5bbf2360ef36fbf883d65298
margub9363 <margub.9363@gmail.com>
2021-06-09 07:50:49 +0530

    142 express rate limit

--- complete-node-bootcamp-master/4-natours/app.js
+++ complete-node-bootcamp-master/4-natours/app.js
@@ -6,12 +6,20 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const rateLimit = require('express-rate-limit');
 
-// 1) Middleware
+// 1)Global Middleware
 // console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again in an hour',
+});
+app.use('/api', limiter);
 
 app.use(express.json()); //middleware
 // app.use(express.static(`${__dirname}/public`));
