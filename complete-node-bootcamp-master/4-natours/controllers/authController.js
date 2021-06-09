a3aa9cdbd9cedcaf7b214c6102ddf7964d361e83
margub9363 <margub.9363@gmail.com>
2021-06-09 12:27:01 +0530

    151

--- complete-node-bootcamp-master/4-natours/controllers/authController.js
+++ complete-node-bootcamp-master/4-natours/controllers/authController.js
@@ -5,7 +5,6 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const sendEmail = require('../utils/email');
-const rateLimit = require('express-rate-limit');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
--- complete-node-bootcamp-master/4-natours/models/tourModel.js
+++ complete-node-bootcamp-master/4-natours/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator'); //in lect  108 we used this , but i ma ignoring it for now
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -75,6 +76,32 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      // GeoLocation
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          ennum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    // guides: Array,
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   },
   {
     toJSON: { virtuals: true },
@@ -91,6 +118,12 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => User.findById(id));
+//   await Promise.all(guidesPromises);
+//   next();
+// });
 
 // tourSchema.pre('save', function (next) {
 //   console.log('Will save document');
