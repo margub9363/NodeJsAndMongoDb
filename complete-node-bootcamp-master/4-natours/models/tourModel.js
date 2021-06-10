18aa0edf1317994cce4d3292ad61c1c16b9330cb
margub9363 <margub.9363@gmail.com>
2021-06-10 16:49:03 +0530

    170 & 171

--- complete-node-bootcamp-master/4-natours/models/tourModel.js
+++ complete-node-bootcamp-master/4-natours/models/tourModel.js
@@ -113,6 +113,7 @@ const tourSchema = new mongoose.Schema(
 // tourSchema.index({ price: 1 });
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' });
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
@@ -169,11 +170,13 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 
 // Aggregation middleware
+/*
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   // console.log(this.pipeline());
   next();
 });
+*/
 
 const Tour = mongoose.model('Tour', tourSchema);
 
