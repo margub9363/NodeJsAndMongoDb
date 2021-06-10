b9f6e61ef503729faee5f4e51a9d43daed1f11e6
margub9363 <margub.9363@gmail.com>
2021-06-10 12:55:38 +0530

    169

--- complete-node-bootcamp-master/4-natours/models/reviewModel.js
+++ complete-node-bootcamp-master/4-natours/models/reviewModel.js
@@ -34,6 +34,8 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
   //   path: 'tour',
@@ -85,7 +87,7 @@ reviewSchema.post('save', function (next) {
 
 reviewSchema.pre(/^findOneAnd/, async function (next) {
   this.r = await this.findOne();
-  // console.log(this.r);
+  // console.log( this.r);
   next();
 });
 
